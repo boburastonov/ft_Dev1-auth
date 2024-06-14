bfbd33d718b1bd4a45dcf48cf220ef7020e81747
boburastonov <boburastonov33@gmail.com>
2024-06-14 10:18:58 +0500

    Initial commit

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { App } from "./components";
+import  App  from "./components/app/app";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
